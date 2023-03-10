d43096b9bd384c9e751f887d6996f9bf8df6b639
Breno Barbosa <brenocb00@outlook.com>
2023-03-09 22:13:26 -0300

    alteração main

--- src/ExemploLexer.java
+++ src/ExemploLexer.java
@@ -1,21 +1,51 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.CharStream ;
+import org.antlr.v4.runtime.CharStreams ;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree ;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
 
 import java.io.IOException;
 
 public class ExemploLexer {
 
     public static void  main (String[] args){
-        String filename = "D:\\Projects\\TrabalhoPraticoGCC130\\codigo.txt";
+        //parsing the input
+        GrammarParser parser = getParser("D:\\ERE5\\novotrabcomp\\TrabalhoPraticoGCC130\\codigo.txt");
+
+        //obter arvore
+        ParseTree ast = parser.programa();
+
+        System.out.println(ast.toStringTree());
+
+        //Inicia o MyListener, nossa implementação do baseListener
+        GrammarBaseListener listener = new GrammarBaseListener();
+
+        ParseTreeWalker walker = new ParseTreeWalker();
+
+        //percorre a ast com a nossa implementação do listener
+        walker.walk(listener,ast);
+
+        //imprime a tabela de símbolos
+        System.out.println(listener.getTabelaSimbolos().toString());
+    }
+
+    //método para executar o lexer e o parser sobre o arquivo de entrada
+    private static GrammarParser getParser(String fileName){
+        GrammarParser parser = null;
         try{
-            CharStream input = CharStreams.fromFileName(filename);
+            CharStream input = CharStreams.fromFileName(fileName);
             GrammarLexer lexer = new GrammarLexer(input);
             CommonTokenStream tokens = new CommonTokenStream(lexer);
-            GrammarParser parser = new GrammarParser(tokens);
-            ParseTree ast = parser.programa();
-            System.out.println(ast.toStringTree());
-        } catch (IOException e){
+            parser = new GrammarParser(tokens);
+
+        } catch(IOException e){
             e.printStackTrace();
         }
+        return parser;
     }
+
+
 }
